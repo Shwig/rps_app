f3a063e55eded74076cc27b3f12aad6a9b7c71ab
Matthew O'Mara <omara.matthew1@gmail.com>
2018-12-20 21:22:25 -0500

    test

--- rps.py
+++ rps.py
@@ -22,14 +22,14 @@ def main():
    #     The winner is the best of 3 rounds.                    #
    #     Good luck!                                             #
    #                                                            #
-   ##############################################################   
+   ##############################################################
        """
-    
+
     score = """
                Score
         ####################
-         Player     CPU   
-           {:d}         {:d}  
+         Player     CPU
+           {:d}         {:d}
         ####################
     """
 
@@ -47,12 +47,12 @@ def main():
         Total rounds: {:d}
          Draw rounds: {:d}
               Draw %: {:.{prec}f}
-        
+
         Player wins: {:d}
               win %: {:.{prec}f}
          Rounds won: {:d}
             round %: {:.{prec}f}
-         
+
         Cpu wins: {:d}
            win %: {:.{prec}f}
         Rnds won: {:d}
@@ -60,7 +60,7 @@ def main():
         ###################
     """
 
-    weapons = ["Rock", "Paper", "Scissors"]    
+    weapons = ["Rock", "Paper", "Scissors"]
 
     # 2D array to hold all possible results
     n = 3
@@ -82,7 +82,7 @@ def main():
     ##############################################################
 
     print(description)
-    
+
     # run the game until the user decides when to quit
     while True:
 
@@ -91,14 +91,14 @@ def main():
         while((playerRounds < 2) and (cpuRounds < 2)):
             print (score.format(playerRounds, cpuRounds))
 
-            # ask the user to choose a weapon and validate their input 
+            # ask the user to choose a weapon and validate their input
             while True:
                 playerChoice = input("Make a selection: Rock (R), Scissors(S), Paper(P) >> ")
                 if playerChoice.lower() not in ('r', 'p', 's'):
                     print("!Not a valid entry! valid entries: r, p, or s ")
                 else:
                     break
-                
+
             # a random weapon is selected for the cpu from the tuple
             cpuChoice = random.choice(('r', 'p', 's'))
 
@@ -106,14 +106,14 @@ def main():
             # the resulting two character string will determine the winner
             matchUp = playerChoice + cpuChoice
 
-            # comment out time.sleep(1) for faster gameplay 
+            # comment out time.sleep(1) for faster gameplay
             # count down for dramatic effect
             print("\n   --Rock!")
             time.sleep(1)
             print("    -Paper!")
             time.sleep(1)
             print("     Scissors!")
-            time.sleep(1)        
+            time.sleep(1)
 
             # display the player and cpu choices
             if playerChoice == 'r':
@@ -128,17 +128,17 @@ def main():
             elif cpuChoice == 'p':
                 cpuWeapon = weapons[1]
             else:
-                cpuWeapon = weapons[2]            
-            
+                cpuWeapon = weapons[2]
+
             #display the chosen weappons on the stage
             print (stage.format(playerWeapon,cpuWeapon,align='^', width='10'))
 
             # compare the current matchUp to the possible results
             # Draw
-            if (matchUp == results[0][0] or matchUp == results[1][0] or matchUp == results[2][0]): 
+            if (matchUp == results[0][0] or matchUp == results[1][0] or matchUp == results[2][0]):
                 print ('{:{align}{width}}'.format('DRAW.', align='^', width='21'))
                 totalDraws += 1
-                
+
             #player wins
             elif (matchUp == results[0][1] or matchUp == results[1][1] or matchUp == results[2][1]):
                 print ('{:{align}{width}}'.format('YOU WON!', align='^', width='25'))
@@ -147,9 +147,9 @@ def main():
 
             #cpu wins
             else:
-                print ('{:{align}{width}}'.format('YOU LOST!', align='^', width='25')) 
+                print ('{:{align}{width}}'.format('YOU LOST!', align='^', width='25'))
                 cpuRounds += 1
-                cpuTotalRounds += 1                
+                cpuTotalRounds += 1
 
             roundCount += 1
             totalRounds += 1
@@ -159,8 +159,8 @@ def main():
             print('')
             playerGames += 1
             print (score.format(playerRounds, cpuRounds))
-            print('YOU WON THE GAME.')            
-            
+            print('YOU WON THE GAME.')
+
         else:
             print('')
             print('')
@@ -185,24 +185,24 @@ def main():
             cpuRoundPercent = cpuTotalRounds/totalRounds
 
             drawPercent = totalDraws/totalRounds
-            
+
             print(stats.format(totalGames, totalRounds, totalDraws, drawPercent,\
                                playerGames, playerGamePercent, playerTotalRounds,\
                                playerRoundPercent,cpuGames, cpuGamePercent,\
                                cpuTotalRounds, cpuRoundPercent, width=5, prec=2))
             input('>>> Hit any key to end session')
             break
-            
+
         # if yes increment game counter and reset counters for best of 3 rounds
-        # if this isn't done counters will be left with the result of the previous game 
+        # if this isn't done counters will be left with the result of the previous game
         # This breaks the second while loop condition causing the game to end instantly
         else:
             totalGames += 1
-            
+
             playerRounds = 0
             cpuRounds = 0
             roundCount = 0
 
 ################################################################
 if __name__ == "__main__":
-    main()
+    main()    
